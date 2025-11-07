b7230034d17b1bf2feeff5bd6a921f7e5bc90862
Amal Sujith <90173181+AmalSujith7@users.noreply.github.com>
2025-11-07 16:00:34 -0500

    Delete OAuth-2.0-Implementation-main directory

--- OAuth-2.0-Implementation-main/auth_server_frontend/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-
-app = FastAPI()
-origins = ["*"]
-app.mount("/site", StaticFiles(directory="./static", html=True), name="site")
